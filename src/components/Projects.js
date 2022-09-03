e840f89f81b1f2e73459236c9ae26887bbc24cd6
Steven Chye <z5257742@ad.unsw.edu.au>
2022-09-03 14:58:45 +1000

    Fixed projects display

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -53,7 +53,7 @@ export default function Projects () {
                     {projects.map((item, key) => {
                         return (
                             <a href={item.link} target="_blank" rel="noreferrer">
-                                <div className="overflow-hidden aspect-video cursor-pointer rounded-xl relative group border" key={key}>
+                                <div className="overflow-hidden cursor-pointer rounded-xl relative group border" key={key}>
                                     <div className="rounded-xl z-50 opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out cursor-pointer absolute from-black/80 to-transparent bg-gradient-to-t inset-x-0 -bottom-2 pt-30 text-white flex items-end">
                                         <div className="transform-gpu p-4 space-y-3 text-xl group-hover:opacity-100 group-hover:translate-y-0 translate-y-4 pb-10 transform transition duration-300 ease-in-out">
                                             <div className="font-bold">{item.name}</div>
